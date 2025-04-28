03cbefd4646bb978feaa1da6bed4590b41542719
Kelvinkimotho <kelvnkwaruingi@gmail.com>
2025-04-28 13:41:48 -0400

    set up the connection to my sqlserver

--- Question-2-CRUD-API/app/database.py
+++ Question-2-CRUD-API/app/database.py
@@ -9,7 +9,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Dependency
+# dependency
 def get_db():
     db = SessionLocal()
     try:
